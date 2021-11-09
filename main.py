37e5d6f0cf350fd0ac07371b0d3639c0c18a5fe1
nandilath <snandhilath@gmail.com>
2021-11-09 21:11:21 +0100

    fiels

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# taking input from user
-number = int(input("Enter any number: "))
+# Python program to check
+# if a string is binary or not
 
-# prime number is always greater than 1
-if number > 1:
-    for i in range(2, number):
-        if (number % i) == 0:
-            print(number, "is not a prime number")
-            break
+# function for checking the
+# string is accepted or not
+def check(string):
+    # set function convert string
+    # into set of characters .
+    p = set(string)
+
+    # declare set of '0', '1' .
+    s = {'0', '1'}
+
+    # check set p is same as set s
+    # or set p contains only '0'
+    # or set p contains only '1'
+    # or not, if any one condition
+    # is true then string is accepted
+    # otherwise not .
+    if s == p or p == {'0'} or p == {'1'}:
+        print("Yes")
     else:
-        print(number, "is a prime number")
+        print("No")
 
-# if the entered number is less than or equal to 1
-# then it is not prime number
-else:
-    print(number, "is not a prime number")
+
+# driver code
+if __name__ == "__main__":
+    string = "101010000111"
+
+    # function calling
+    check(string)
